0e8d261f2bb2549176678e6150af47c85b9970df
Ryan Handerhan <rhanderh@gmail.com>
2014-06-01 18:07:04 -0700

    Setup PostgreSQL DB and Started Initial App

--- ClimateAnalytics/settings.py
+++ ClimateAnalytics/settings.py
@@ -57,8 +57,13 @@ WSGI_APPLICATION = 'ClimateAnalytics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'climateanalytics',
+        'USER': 'climateuser',
+        'PASSWORD': 'climate',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    
     }
 }
 
