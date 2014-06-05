da18cfd1ef2f083360012c44983f54f4eee3edc8
Ryan Handerhan <rhanderh@gmail.com>
2014-06-04 22:36:41 -0700

    added temp and loc basic models, sync db, and registered with admin

--- climate/admin.py
+++ climate/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from climate.models import Temperature, Location
+
 
 # Register your models here.
+admin.site.register(Temperature)
+admin.site.register(Location)
--- climate/models.py
+++ climate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    city_id = models.CharField(max_length=50)
+    city_name = models.CharField(max_length=200)
+    population = models.IntegerField()
+
+class Temperature(models.Model):
+    temp_min = models.DecimalField(max_digits=9,decimal_places=4)
+    temp_max = models.DecimalField(max_digits=9,decimal_places=4)
+    temp = models.DecimalField(max_digits=9,decimal_places=4)
+    timestamp = models.DateTimeField(auto_now_add=False)
+    location = models.ForeignKey(Location)
+    
+
+    
