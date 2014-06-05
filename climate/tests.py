0a529937688444268c9dd05ed60dcf1a7bd9131e
Ryan Handerhan <rhanderh@gmail.com>
2014-06-05 00:31:59 -0700

    Created first OpenWeather call function and First test to validate call success

--- climate/tests.py
+++ climate/tests.py
@@ -1,3 +1,17 @@
+#Django Imports
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+#Application Imports
+from climate.ETL import LocationLoader
+
+class LocationLoaderTest(TestCase):
+    
+    #Validate successful call of weather data by city
+    def test_get_open_weather_city_call(self):
+        loc_loader = LocationLoader()
+        data = loc_loader.get_open_weather_city('Los Angeles,ca')
+        self.assertEqual(data[u'name'], u'Los Angeles')
