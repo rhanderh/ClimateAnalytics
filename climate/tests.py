184a30aff62f5ef141bcb2756d62834e60d9e567
Ryan Handerhan <rhanderh@gmail.com>
2014-06-08 11:14:07 -0700

    completed initial LocationLoader test cases

--- climate/tests.py
+++ climate/tests.py
@@ -7,33 +7,59 @@ from django.core.urlresolvers import reverse
 
 #Application Imports
 from climate.ETL import LocationLoader
+from climate.models import Location,Temperature
 
 class LocationLoaderTest(TestCase):
     
-    #Validate successful call of weather data by city
+    
     def test_get_open_weather_city(self):
+        print('Validate successful call of weather data by city')
         loc_loader = LocationLoader()
         data = loc_loader.get_open_weather_city('Los Angeles,ca')
         print('Data Received from API Call')
         print(data)
         self.assertEqual(data[u'name'], u'Los Angeles')
         
-    #Validate successful call of METRIC weather data by city
+    
     def test_get_open_weather_city_metric(self):
+        print('Validate successful call of METRIC weather data by city')
         loc_loader = LocationLoader()
         data = loc_loader.get_open_weather_city_metric('Los Angeles,ca')
         print('Data Received from API Call')
         print(data)
         self.assertEqual(data[u'name'], u'Los Angeles')
         
-    #Validate successful call of IMPERIAL weather data by city
+   
     def test_get_open_weather_city_imperial(self):
+        print('Validate successful call of IMPERIAL weather data by city')
         loc_loader = LocationLoader()
         data = loc_loader.get_open_weather_city_metric('Los Angeles,ca')
         print('Data Received from API Call')
         print(data)
         self.assertEqual(data[u'name'], u'Los Angeles')
         
-    #Validate what is saved to the database matches what is received by JSON
+ 
+    def test_save_data_city(self):
+        print('Validate what is saved to the database matches what is received by JSON')
+        #Current count of location and temp set for that location
+        if Location.objects.filter(city_name='Pittsburgh').exists():
+            Pitt = Location.objects.filter(city_name='Pittsburgh')
+            Temp = Temperature.objects.get(location=Pitt)
+            Orig_count = Temp.count()
+        else:
+            Orig_count = 0
+        
+        #Save a new entry
+        loc_loader = LocationLoader()
+        loc_loader.save_data_city('Pittsburgh,pa')
+        
+        #Assert location and tempset have been entered
+        Pitt = Location.objects.get(city_name='Pittsburgh')
+        Temp_count = Pitt.temperature_set.all().count()
+        self.assertTrue(Location.objects.filter(city_name='Pittsburgh').exists())
+        self.assertTrue(Temp_count == Orig_count + 1)
+       
+        
+        
     
         
