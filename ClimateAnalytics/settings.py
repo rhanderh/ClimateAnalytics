df70358afc8e9c15bf537f8fff71efe0b58b2a4a
Ryan Handerhan <rhanderh@gmail.com>
2014-06-17 00:37:57 -0700

    fix eb static attempt

--- ClimateAnalytics/settings.py
+++ ClimateAnalytics/settings.py
@@ -86,3 +86,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
+
