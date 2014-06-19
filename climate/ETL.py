30f1c451ab2f1266c772685e2b10fdd27f07aa0f
Ryan Handerhan <rhanderh@gmail.com>
2014-06-19 14:49:43 -0700

    refactored out save city data to a separate method to limit repetition, added save_data_city_hist [draft]

--- climate/ETL.py
+++ climate/ETL.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from decimal import Decimal
 import json
 import urllib2
+import datetime
 
 class LocationLoader():
 
@@ -32,54 +33,10 @@ class LocationLoader():
         metric_data = self.get_open_weather_city_metric(name)
         imperial_data = self.get_open_weather_city_imperial(name)
         
-        #Prep model objects
-        loc = Location()
-        temp_data = Temperature()
-        wind_data = WindSpeed()
-
-        #Assign location fields from collected json data
-        loc.longitude = data[u'coord'][u'lon']
-        loc.latitude = data[u'coord'][u'lat']
-        loc.city_id = data[u'id']
-        loc.city_name = data[u'name']
+        data_save = DataSaverLocation()
         
-        #Insert Location if it does not already exist
-        
-        try:
-            comp = Location.objects.get(city_id = data[u'id'])
-        except:
-            loc.save()
-        else:   
-            print "location exists"
-        
-        
-        #Assign temperature fields from collected json data
-        temp_data.temp_min = Decimal(data[u'main'][u'temp_min'])
-        temp_data.temp_max = Decimal(data[u'main'][u'temp_max'])
-        temp_data.temp = Decimal(data[u'main'][u'temp'])
-        temp_data.temp_min_imperial = Decimal(imperial_data[u'main'][u'temp_min'])
-        temp_data.temp_max_imperial = Decimal(imperial_data[u'main'][u'temp_max'])
-        temp_data.temp_imperial = Decimal(imperial_data[u'main'][u'temp'])
-        temp_data.temp_min_metric = Decimal(metric_data[u'main'][u'temp_min'])
-        temp_data.temp_max_metric = Decimal(metric_data[u'main'][u'temp_max'])
-        temp_data.temp_metric = Decimal(metric_data[u'main'][u'temp'])
-        temp_data.location = Location.objects.get(city_id = loc.city_id)
-        
-        #Save temperature data
-        temp_data.save()
-        
-        #Assign windspeed fields from collected json data
-        wind_data.location = Location.objects.get(city_id = loc.city_id)
-        wind_data.wind_speed = Decimal(data[u'wind'][u'speed'])
-        wind_data.degree = Decimal(data[u'wind'][u'deg'])
-        wind_data.wind_speed_imperial = Decimal(imperial_data[u'wind'][u'speed'])
-        wind_data.degree_imperial = Decimal(imperial_data[u'wind'][u'deg'])
-        wind_data.wind_speed_metric = Decimal(metric_data[u'wind'][u'speed'])
-        wind_data.degree_metric = Decimal(metric_data[u'wind'][u'deg'])
-        
-        #Save windspeed data
-        wind_data.save()
-            
+        data_save.commit_data(data,metric_data,imperial_data)
+           
             
 class LocationLoaderHist():       
         
@@ -95,5 +52,70 @@ class LocationLoaderHist():
             data = json.load(urllib2.urlopen('http://api.openweathermap.org/data/2.5/history/city?q=' + name + '&type=day' + '&units=imperial'))
             return data 
         
+        def save_data_city_hist(self, name):
         
+        #Call weather api's to collect data
+            data = self.get_open_weather_city_hist(name)
+            metric_data = self.get_open_weather_city_hist_metric(name)
+            imperial_data = self.get_open_weather_city_hist_imperial(name)
             
+            data_save = DataSaverLocation()
+            
+            i=0
+            while i <= len(data):
+                data_save.commit_data(data[u'list'].index(i),metric_data[u'list'].index(i),imperial_data[u'list'].index(i))
+                i = i + 1
+            
+class DataSaverLocation():           
+        def commit_data(self,data,metric_data,imperial_data):
+            
+        
+        #Prep model objects
+            loc = Location()
+            temp_data = Temperature()
+            wind_data = WindSpeed()
+
+        #Assign location fields from collected json data
+            loc.longitude = data[u'coord'][u'lon']
+            loc.latitude = data[u'coord'][u'lat']
+            loc.city_id = data[u'id']
+            loc.city_name = data[u'name']
+        
+        #Insert Location if it does not already exist
+        
+            try:
+                comp = Location.objects.get(city_id = data[u'id'])
+            except:
+                loc.save()
+            else:   
+                print "location exists"
+        
+        
+        #Assign temperature fields from collected json data
+            temp_data.temp_min = Decimal(data[u'main'][u'temp_min'])
+            temp_data.temp_max = Decimal(data[u'main'][u'temp_max'])
+            temp_data.temp = Decimal(data[u'main'][u'temp'])
+            temp_data.temp_min_imperial = Decimal(imperial_data[u'main'][u'temp_min'])
+            temp_data.temp_max_imperial = Decimal(imperial_data[u'main'][u'temp_max'])
+            temp_data.temp_imperial = Decimal(imperial_data[u'main'][u'temp'])
+            temp_data.temp_min_metric = Decimal(metric_data[u'main'][u'temp_min'])
+            temp_data.temp_max_metric = Decimal(metric_data[u'main'][u'temp_max'])
+            temp_data.temp_metric = Decimal(metric_data[u'main'][u'temp'])
+            temp_data.location = Location.objects.get(city_id = loc.city_id)
+        
+        #Save temperature data
+            temp_data.save()
+        
+        #Assign windspeed fields from collected json data
+            wind_data.location = Location.objects.get(city_id = loc.city_id)
+            wind_data.wind_speed = Decimal(data[u'wind'][u'speed'])
+            wind_data.degree = Decimal(data[u'wind'][u'deg'])
+            wind_data.wind_speed_imperial = Decimal(imperial_data[u'wind'][u'speed'])
+            wind_data.degree_imperial = Decimal(imperial_data[u'wind'][u'deg'])
+            wind_data.wind_speed_metric = Decimal(metric_data[u'wind'][u'speed'])
+            wind_data.degree_metric = Decimal(metric_data[u'wind'][u'deg'])
+        
+        #Save windspeed data
+            wind_data.save()
+        
+        
