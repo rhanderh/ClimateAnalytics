6189e1157ce3f9d93fc980a99fea096a1c2812d3
System Administrator <root@eduardos-iMac.local>
2014-07-20 08:56:03 -0400

    Modified forecast view and html to calculate gradient and dynamic visual of city temperature.

--- climate/views.py
+++ climate/views.py
@@ -23,16 +23,56 @@ def CityHistoryGraphs(request, id):
 def ForecastGraphs(request, id):
     try:
         location = Location.objects.get(pk=id)
+        NY = Location.objects.get(city_name='New York')
+        LA = Location.objects.get(city_name='Los Angeles')
+        HOU = Location.objects.get(city_name='Houston')
+        MIA = Location.objects.get(city_name='Miami')
+        PHI = Location.objects.get(city_name='Philadelphia')
+        BOS = Location.objects.get(city_name='Boston')
+        SEA = Location.objects.get(city_name='Seattle')
+        DEN = Location.objects.get(city_name='Denver')
+        SF = Location.objects.get(city_name='San Francisco')
         windspeed = WindSpeed.objects.get(pk=id)
         advection = Advection.objects.get(pk=id)
         json_temp = serializers.serialize("json", location.temperature_set.all())
+        jsonNY    = serializers.serialize("json", NY.temperature_set.all())        
+        jsonLA    = serializers.serialize("json", LA.temperature_set.all())
+        jsonHOU    = serializers.serialize("json", HOU.temperature_set.all())
+        jsonMIA    = serializers.serialize("json", MIA.temperature_set.all())
+        jsonPHI    = serializers.serialize("json", PHI.temperature_set.all())
+        jsonBOS    = serializers.serialize("json", BOS.temperature_set.all())
+        jsonSEA    = serializers.serialize("json", SEA.temperature_set.all())
+        jsonDEN    = serializers.serialize("json", DEN.temperature_set.all())
+        jsonSF    = serializers.serialize("json",  SF.temperature_set.all())
         json_ctemp = serializers.serialize("json", location.temperature_set.all())
+
+        jsonNYct = serializers.serialize("json", NY.temperature_set.all())
+        jsonLAct = serializers.serialize("json", LA.temperature_set.all())
+        jsonHOUct = serializers.serialize("json", HOU.temperature_set.all())
+        jsonMIAct = serializers.serialize("json", MIA.temperature_set.all())
+        jsonPHIct = serializers.serialize("json", PHI.temperature_set.all())
+        jsonBOSct = serializers.serialize("json", BOS.temperature_set.all())
+        jsonSEAct = serializers.serialize("json", SEA.temperature_set.all())
+        jsonDENct = serializers.serialize("json", DEN.temperature_set.all())
+        jsonSFct = serializers.serialize("json", SF.temperature_set.all())
+        try:
+           json_gradient = (float(json_temp[259:266])-float(5.000))/100
+        except ValueError:
+           json_gradient = (float(json_temp[256:262])-float(5.000))/100
+  
         json_wind = serializers.serialize("json", WindSpeed.objects.filter(location_id=id), fields=('wind_speed'), indent =3)
         json_advection = serializers.serialize("json", Advection.objects.filter(location_id=id), fields=('advection'), indent =2)
-        json_temp = float(json_temp[256:262])+float(json_advection[81:85])
+        try:
+           json_temp = float(json_temp[259:266])+float(json_advection[81:85])
+        except ValueError:
+           json_temp = float(json_temp[256:262])+float(json_advection[81:85])
     except Location.DoesNotExist:
         raise Http404
-    return render(request,'climate/forecastdetail.html',{'location': location, 'json_temp': json_temp , 'windspeed': json_wind[87:92],'advection': json_advection[81:85], 'json_ctemp' : json_ctemp[256:262] })
+    return render(request,'climate/forecastdetail.html',{'location': location, 'json_temp': json_temp , 'windspeed': json_wind[87:92],'advection': json_advection[81:85], 'json_ctemp' : json_ctemp[256:262],
+                                                          'json_gradient' : json_gradient, 'jsonNYct': jsonNYct[259:266],'jsonLAct': jsonLAct[256:262], 
+                                                           'jsonHOUct': jsonHOUct[256:262], 'jsonMIAct': jsonMIAct[255:262],'jsonPHIct': jsonPHIct[256:262],
+                                                           'jsonBOSct': jsonBOSct[256:262], 'jsonSEAct': jsonSEAct[256:262], 'jsonDENct': jsonDENct[255:262], 
+                                                             })
 
     
 def HistoryGraph(id):
