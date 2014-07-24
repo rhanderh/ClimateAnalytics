844465977596c353fd5ba3ab36bde6aade2562ed
System Administrator <root@eduardos-iMac.local>
2014-07-23 21:47:46 -0400

    retrieve forecast changes

--- climate/views.py
+++ climate/views.py
@@ -6,6 +6,7 @@ import json
 from django.http import Http404
 from django.views import generic
 from django.db.models import Max, Min
+from django.db.models import Max
 
 from django import forms
 from django.shortcuts import render
@@ -130,19 +131,19 @@ def ForecastGraphs(request, id):
         #Location for user selection
         location = Location.objects.get(pk=id)
        
-        
+        max_time = location.temperature_set.all().aggregate(Max('timestamp'))
+        temp = location.temperature_set.get(timestamp=max_time['timestamp__max'])
         
         #Locations for current temps in other cities
         NY = Location.objects.get(city_name='New York')
         LA = Location.objects.get(city_name='Los Angeles')
-        HOU = Location.objects.get(city_name='Houston')
         MIA = Location.objects.get(city_name='Miami')
         PHI = Location.objects.get(city_name='Philadelphia')
         BOS = Location.objects.get(city_name='Boston')
         SEA = Location.objects.get(city_name='Seattle')
         DEN = Location.objects.get(city_name='Denver')
         SF = Location.objects.get(city_name='San Francisco')
-        
+        HOU = Location.objects.get(city_name='Houston')        
         windspeed = WindSpeed.objects.get(pk=id)
         advection = Advection.objects.get(pk=id)
         
@@ -150,31 +151,13 @@ def ForecastGraphs(request, id):
         #Do Math on Python object first before converting to JSON
         
         json_temp = serializers.serialize("json", location.temperature_set.all())
-        
-        
+                
+               
         #Don't grab the full temperature sets here - just grab the latest temp for today from the DB
 
-        jsonNY    = serializers.serialize("json", NY.temperature_set.all())        
-        jsonLA    = serializers.serialize("json", LA.temperature_set.all())
-        jsonHOU    = serializers.serialize("json", HOU.temperature_set.all())
-        jsonMIA    = serializers.serialize("json", MIA.temperature_set.all())
-        jsonPHI    = serializers.serialize("json", PHI.temperature_set.all())
-        jsonBOS    = serializers.serialize("json", BOS.temperature_set.all())
-        jsonSEA    = serializers.serialize("json", SEA.temperature_set.all())
-        jsonDEN    = serializers.serialize("json", DEN.temperature_set.all())
-        jsonSF    = serializers.serialize("json",  SF.temperature_set.all())
         
         json_ctemp = serializers.serialize("json", location.temperature_set.all())
 
-        jsonNYct = serializers.serialize("json", NY.temperature_set.all())
-        jsonLAct = serializers.serialize("json", LA.temperature_set.all())
-        jsonHOUct = serializers.serialize("json", HOU.temperature_set.all())
-        jsonMIAct = serializers.serialize("json", MIA.temperature_set.all())
-        jsonPHIct = serializers.serialize("json", PHI.temperature_set.all())
-        jsonBOSct = serializers.serialize("json", BOS.temperature_set.all())
-        jsonSEAct = serializers.serialize("json", SEA.temperature_set.all())
-        jsonDENct = serializers.serialize("json", DEN.temperature_set.all())
-        jsonSFct = serializers.serialize("json", SF.temperature_set.all())
         
         try:
            json_gradient = (float(json_temp[259:266])-float(5.000))/100
@@ -189,10 +172,7 @@ def ForecastGraphs(request, id):
            json_temp = float(json_temp[256:262])+float(json_advection[81:85])
     except Location.DoesNotExist:
         raise Http404
-    return render(request,'climate/forecastdetail.html',{'location': location, 'json_temp': json_temp , 'windspeed': json_wind[87:92],'advection': json_advection[81:85], 'json_ctemp' : json_ctemp[256:262],
-                                                          'json_gradient' : json_gradient, 'json_temp_range': json_temp_range, 'jsonNYct': jsonNYct[259:266],'jsonLAct': jsonLAct[256:262],'jsonNYct': jsonNYct[259:266],'jsonLAct': jsonLAct[256:262],'jsonHOUct': jsonHOUct[256:262], 'jsonMIAct': jsonMIAct[255:262],'jsonPHIct': jsonPHIct[256:262],
-                                                           'jsonBOSct': jsonBOSct[256:262], 'jsonSEAct': jsonSEAct[256:262], 'jsonDENct': jsonDENct[255:262], 
-                                                           })
+    return render(request,'climate/forecastdetail.html',{'location': location, 'json_temp': json_temp ,'temp':temp, 'windspeed': json_wind[87:92],'advection': json_advection[81:85], 'json_ctemp' : json_ctemp[256:262],'json_gradient' : json_gradient, })
 
     
 def HistoryGraph(id):
